2a7c65a32ab56f4bacdd38ddafc80f7f812250e1
Pablo-Echeverri <pabloecheverria@opendeusto.es>
2024-12-14 19:40:52 +0100

    test update levels success

--- test/test_shelter.py
+++ test/test_shelter.py
@@ -102,3 +102,105 @@ def test_get_shelter_no_shelter_found(setup_database):
 
     with pytest.raises(ValueError, match="No shelter found in the database."):
         controller.get_shelter_radiation_level(session=session)
+        
+
+def test_updateShelterEnergyLevel_success(setup_database, mocker):
+    """
+    Test to verify that the updateShelterEnergyLevel method correctly updates the energy level of a shelter.
+
+    This test simulates the process of updating the energy level of a shelter in the database.
+    It first creates a shelter with an initial energy level of 100, saves it to the database, and then
+    calls the updateShelterEnergyLevel method to update the energy level to 80.
+
+    Steps:
+        1. Add a shelter to the database.
+        2. Call the `updateShelterEnergyLevel` method to update the shelter's energy level.
+        3. Verify the response and the updated energy level in the database.
+    
+    Expected Outcome:
+        - The method returns a success message.
+        - The shelter's energy level is updated in the database.
+    """
+    session = setup_database
+    controller = ShelterController()
+
+    # Add a shelter
+    shelter = Shelter(idShelter=1, shelterName="Main Shelter", maxPeople=50, energyLevel=100, waterLevel=100, radiationLevel=10)
+    session.add(shelter)
+    session.commit()
+
+    # Update the shelter's energy level
+    response = controller.updateShelterEnergyLevel(80, session)
+
+    # Verify the response and databes changes
+    assert response == {"status": "ok", "message": "Nivel de energía actualizado exitosamente"}
+    updated_shelter = session.query(Shelter).filter(Shelter.idShelter == 1).first()
+    assert updated_shelter.energyLevel == 80
+
+
+def test_updateShelterWaterLevel_success(setup_database, mocker):
+    """
+    Test to verify that the updateShelterWaterLevel method correctly updates the water level of a shelter.
+
+    This test simulates the process of updating the water level of a shelter in the database.
+    It first creates a shelter with an initial water level of 100, saves it to the database, and then
+    calls the updateShelterWaterLevel method to update the water level to 80.
+
+    Steps:
+        1. Add a shelter to the database.
+        2. Call the `updateWaterEnergyLevel` method to update the shelter's energy level.
+        3. Verify the response and the updated water level in the database.
+    
+    Expected Outcome:
+        - The method returns a success message.
+        - The shelter's water level is updated in the database.
+    """
+    session = setup_database
+    controller = ShelterController()
+
+    # Add a shelter
+    shelter = Shelter(idShelter=1, shelterName="Main Shelter", maxPeople=50, energyLevel=100, waterLevel=100, radiationLevel=10)
+    session.add(shelter)
+    session.commit()
+
+    # Update the shelter's water level
+    response = controller.updateShelterWaterLevel(80, session)
+
+    # Verify the response and databes changes
+    assert response == {"status": "ok", "message": "Nivel de agua actualizado exitosamente"}
+    updated_shelter = session.query(Shelter).filter(Shelter.idShelter == 1).first()
+    assert updated_shelter.waterLevel == 80
+
+
+def test_updateShelterRadiationLevel_success(setup_database):
+    """
+    Test to verify that the updateShelterRadiationLevel method correctly updates the radiation level of a shelter.
+
+    This test simulates the process of updating the radiation level of a shelter in the database.
+    It first creates a shelter with an initial radiation level of 10, saves it to the database, and then
+    calls the updateShelterRadiationLevel method to update the radiation level to 8.
+
+    Steps:
+        1. Add a shelter to the database.
+        2. Call the `updateShelterRadiationLevel` method to update the shelter's radiation level.
+        3. Verify the response and the updated radiation level in the database.
+    
+    Expected Outcome:
+        - The method returns a success message.
+        - The shelter's radiation level is updated in the database.
+    """
+    session = setup_database
+    controller = ShelterController()
+
+    # Add a shelter
+    shelter = Shelter(idShelter=1, shelterName="Main Shelter", maxPeople=50, energyLevel=100, waterLevel=100, radiationLevel=10)
+    session.add(shelter)
+    session.commit()
+
+    # Update the shelter's radiation level
+    response = controller.updateShelterRadiationLevel(8, session)
+
+    # Verify the response and databes changes
+    assert response == {"status": "ok", "message": "Nivel de radiación actualizado exitosamente"}
+    updated_shelter = session.query(Shelter).filter(Shelter.idShelter == 1).first()
+    assert updated_shelter.radiationLevel == 8
